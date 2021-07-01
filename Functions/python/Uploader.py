2016ea07a21b2e8afcae87da32236b13438f8bf6
Patcharapol Thanasuttawong <patcharapol.t@bluepi.co.th>
2021-07-01 13:54:38 +0700

    add billist for debug

--- Functions/python/Uploader.py
+++ Functions/python/Uploader.py
@@ -202,7 +202,7 @@ class Uploader ():
                 not_existing_list.append(bill)
         
         #If any bill not exist return false
-        return_dict = {'not_existing_list': not_existing_list, 'existing_list': existing_list, 'doc_list': doc_list}
+        return_dict = {'not_existing_list': not_existing_list,'bill_list': bill_list, 'existing_list': existing_list, 'doc_list': doc_list}
         
         if 'failed' in is_sucess_list:
             
