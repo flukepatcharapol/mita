02a2fbdeacbccc39ec9fa62a4f414674281d47ce
Patcharapol Thanasuttawong <patcharapol.t@bluepi.co.th>
2021-07-20 19:21:49 +0700

    fix

--- Functions/python/Uploader.py
+++ Functions/python/Uploader.py
@@ -307,10 +307,11 @@ class Uploader ():
         used_time_obj = datetime.strptime(used_due_date, '%d-%m-%Y')
         expired_time_obj = datetime.strptime(expire_date, '%d-%m-%Y')
         expired = db.collection('RedeemHistory').where('ExpiredDate', '<=', expired_time_obj).get()
+        # used = db.collection('RedeemHistory').where('UsedDate', '<=', used_due_date).get()
         expired_list = []
         for doc in expired:
-            if (doc.UsedDate == None):
-                expired_list.append('{doc.id} => {doc.to_dict}')
+            # if (doc.UsedDate == None):
+            expired_list.append('{doc.id} => {doc.to_dict}')
         return expired_list
             
         # used = db.collection('RedeemHistory').where('UsedDate', '<=', used_time_obj).get()
